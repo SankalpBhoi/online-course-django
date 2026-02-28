c845eae0bef2b78877d4bbe68211c6f577956692
SankalpBhoi <sankalpbhoi5854@gmail.com>
2026-02-28 14:49:08 +0530

    Final Online Course Project

---
+++ course/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import Course, Lesson, Question, Choice, Submission
+from django.utils.html import format_html
+from django.urls import reverse
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'lesson')
+    inlines = [ChoiceInline]
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('title', 'course')
+    inlines = [QuestionInline]
+
+
+admin.site.register(Course)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
---
+++ course/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from .models import Course, Question, Choice, Submission
+
+
+def course_detail(request, course_id):
+    course = Course.objects.get(id=course_id)
+    return render(request, 'courses/course_details_bootstrap.html', {'course': course})
+
+
+def submit(request):
+    if request.method == "POST":
+        score = 0
+        questions = Question.objects.all()
+
+        for question in questions:
+            selected = request.POST.get(str(question.id))
+            if selected:
+                choice = Choice.objects.get(id=selected)
+                if choice.is_correct:
+                    score += 1
+
+        return render(request, 'courses/result.html', {'score': score})
+
+    questions = Question.objects.all()
+    return render(request, 'courses/exam.html', {'questions': questions})
+
+
+def show_exam_result(request):
+    submissions = Submission.objects.all()
+    return render(request, 'courses/show_result.html', {'submissions': submissions})
